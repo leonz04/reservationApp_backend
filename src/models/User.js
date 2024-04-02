56557602888382cc05b79f76e14627ccc1190b35
afleonz04 <afleonz04@gmail.com>
2024-04-01 19:21:22 -0500

    hide pasword

--- src/models/User.js
+++ src/models/User.js
@@ -34,10 +34,10 @@ const User = sequelize.define('user', {
     }
 });
 
-// User.prototype.toJSON = function () {
-//     const values = Object.assign({}, this.get());
-//     delete values.password;
-//     return values;
-// }
+User.prototype.toJSON = function () {
+    const values = Object.assign({}, this.get());
+    delete values.password;
+    return values;
+}
 
 module.exports = User;
